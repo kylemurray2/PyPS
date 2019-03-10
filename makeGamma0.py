57a257e7d47cfe4104e95fdca7231780d48bc651
kdm95 <kdm95@skunk.geo.cornell.edu>
2019-03-10 17:14:12 -0400

    version 1.0.2

--- makeGamma0.py
+++ makeGamma0.py
@@ -15,16 +15,17 @@ Saves downlooked ifgs in the respective ifg directories.
 
 import numpy as np
 import isceobj
-import pickle
 from matplotlib import pyplot as plt
 import cv2
 import os
 #from mroipac.filter.Filter import Filter
 
-with open(tsdir + 'params.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
-    pairs,nd,lam,workdir,intdir,tsdir,ny,nx,nxl,nyl,alks,rlks = pickle.load(f)
+params = np.load('params.npy').item()
+locals().update(params)
+
 
 # Make the gaussian filter we'll convolve with ifg
+skip = 3 # how many redundant pairs did you make?
 rx = 5
 ry = 5
 rx2 = np.floor(rx*3)
@@ -35,11 +36,12 @@ gaus = gausx[:, np.newaxis] * gausy[np.newaxis, :]
 gaus -= gaus.min()
 gaus  /= np.sum(gaus.flatten())
 
-for pair in pairs:
-    if not os.path.isfile(intdir + pair + '/fine_diff.int'):
+
+for pair in params['pairs'][0:len(params['pairs']):skip]: 
+    if not os.path.isfile(params['intdir'] + '/' + pair + '/fine_diff.int'):
         print('working on ' + pair)
         #load ifg real and imaginary parts
-        f = intdir + pair + '/fine.int'
+        f = params['intdir'] +'/'+ pair + '/fine.int'
         intImage = isceobj.createIntImage()
         intImage.load(f + '.xml')
         ifg_real = np.real(intImage.memMap()[:,:,0] )
@@ -67,9 +69,9 @@ for pair in pairs:
         #save diff ifg
         out = intImage.clone() # Copy the interferogram image from before
         out.scheme =  'BIP' #'BIP'/ 'BIL' / 'BSQ' 
-        out.filename = intdir + pair + '/fine_diff.int'
-        out.dump(intdir + pair + '/fine_diff.int.xml') # Write out xml
-        phsdiff.tofile(intdir+pair+'/fine_diff.int') # Write file out
+        out.filename = params['intdir'] + '/' + pair + '/fine_diff.int'
+        out.dump(params['intdir']  + '/' + pair + '/fine_diff.int.xml') # Write out xml
+        phsdiff.tofile(params['intdir'] + '/' + pair+'/fine_diff.int') # Write file out
 
 
 del(ifg_imag,ifg_imag_filt,ifg_real,ifg_real_filt,phs,cpx0,cpxf,phs_filt,phsdiff)
@@ -77,23 +79,23 @@ del(ifg_imag,ifg_imag_filt,ifg_real,ifg_real_filt,phs,cpx0,cpxf,phs_filt,phsdiff
 gamma0 =list()
 # Make a stack of the diff images (memory mapped )
 # We have to do this in 20 subsections to save on memory
-chunks = np.linspace(0,ny,20,dtype=int)
+chunks = np.linspace(0,params['ny'],20,dtype=int)
 for ii in np.arange(0,len(chunks)-1):
     diff_stack = list()
-    for pair in pairs: #loop through each ifg and save to 
-        diff_file = intdir + pair + '/fine_diff.int'
+    for pair in params['pairs'][0:len(params['pairs']):skip]: #loop through each ifg and save to 
+        diff_file = params['intdir'] + '/' + pair + '/fine_diff.int'
         diffImage = isceobj.createIntImage()
         diffImage.load(diff_file + '.xml')
         diff_stack.append(diffImage.memMap()[chunks[ii]:chunks[ii+1],:,0])
     # Find phase variance 
-    gamma0.append(np.abs( np.nansum( np.asarray(diff_stack), axis=0)/nd )) # easily operate on pixel axis of memory mapped stack :D
+    gamma0.append(np.abs( np.nansum( np.asarray(diff_stack), axis=0)/params['nd'] )) 
 
-a=np.empty((ny,nx))
+a=np.empty((params['ny'],params['nx']))
 for ii in np.arange(0,len(chunks)-1):
     a[chunks[ii]:chunks[ii+1]]=gamma0[ii]
 gamma0 = a
 gamma0 = np.asarray(gamma0)
-gamma0=np.reshape(np.asarray(gamma0),(ny,nx))
+gamma0=np.reshape(np.asarray(gamma0),(params['ny'],params['nx']))
 gamma0[np.where(gamma0==0)]=np.nan
 gamma0=gamma0.astype(np.float32)
 
@@ -101,8 +103,8 @@ gamma0=gamma0.astype(np.float32)
 # Save gamma0 file
 out = intImage.clone() # Copy the interferogram image from before
 out.dataType = 'FLOAT'
-out.filename = tsdir + 'gamma0.int'
-out.dump(tsdir + 'gamma0.int.xml') # Write out xml
+out.filename = params['tsdir'] + '/gamma0.int'
+out.dump(params['tsdir'] + '/gamma0.int.xml') # Write out xml
 gamma0.tofile(out.filename) # Write file out
 
 plt.imshow(gamma0,vmin=0.1,vmax=.8)
